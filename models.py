f573ea4893980bc85cc00bfc04f92520fb19be08
sasiprojs <43777666+sasiprojs@users.noreply.github.com>
2018-10-29 06:22:38 -0400

    Added patient's database

--- models.py
+++ models.py
@@ -4,3 +4,16 @@ from django.db import models
 class latlng(models.Model):
     lat=models.FloatField()
     lng=models.FloatField()
+    
+# patients database
+
+class Patient_DB(models.Model):
+    Name = models.CharField(max_length=200)
+    Age = models.IntegerField
+    Gender = models.CharField(max_length=10)
+    Blood_group = models.CharField(max_length=3)
+    Maritial_Status = models.CharField(max_length=3)
+    Phone_num = models.IntegerField
+    Emergency_num = models.IntegerField
+    PatientId = models.CharField(max_length=255, default='0')
+    Email_Id = models.CharField(max_length=255,default='NULL')
