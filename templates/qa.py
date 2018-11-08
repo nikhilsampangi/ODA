bbc8115529c8772698fb18096c59ea41f11d02ed
Kiran Sankar E J <43779350+KIRANSANKAREJ@users.noreply.github.com>
2018-11-08 21:09:42 +0530

    Add files via upload

---
+++ templates/qa.py
@@ -0,0 +1,17 @@
+qanda = [
+    {
+        'Question': 'What is O.D.A ?',
+        'Answer': 'O.D.A is an Online Doctor Appointment webservice.',
+        'RM': 'blah',
+    },
+    {
+        'Question': 'Why should I use it ?',
+        'Answer': 'You shouldn\'t use it if you\'d rather stand in a long queue for your doctor\'s appointment.',
+        'RM': 'bleh',
+    },
+    {
+        'Question': 'Blah blah blah blah blah ?',
+        'Answer': 'Bluh Bluh Bluh.kjshdfkjhsdkfhwsoseihfoliehjfolihjejoflikshdjolcfikjwswoihjnuijhsfdfikjaolidjfoidoliadoliksoivnoslfdijknvlosdinjvolsiknjnvoliknsdloviknsoldikvnlosdiknlkdvflksdvolkflvknsdfslfkvnlddfkfnvlknvlkdsnfolikmdsfkmvlksfdmdvliksdlfjknsdlkoujvhnolwsuijedhgikuaqjdhfolikujwsdnolfuicjsdolikjfolwseijffnloksdngloujsfksjdhfkjshdfkjshdkfjhsdkjfh',
+        'RM': 'Blih',
+    },
+]
