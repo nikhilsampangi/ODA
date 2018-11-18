5ab990918bb935a0af5dc4cfdc36dd9cc6eb5a26
Yashwanth-kikkuri <37268283+Yashwanth-kikkuri@users.noreply.github.com>
2018-11-18 23:48:57 +0530

    Add files via upload

--- ODA/ODA/urls.py
+++ ODA/ODA/urls.py
@@ -1,4 +1,4 @@
-"""ODA URL Configuration
+"""oda URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('welcome.urls')),
-    path('DandS',include('DandS.urls')),
+    path('', include('welcome.urls', namespace="login")),
+
 ]
