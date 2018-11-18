1f96f1c4ddac6bbccbca9c747803e4f5b7292790
Yashwanth-kikkuri <37268283+Yashwanth-kikkuri@users.noreply.github.com>
2018-11-18 23:47:57 +0530

    updating all files

--- ODA/patient_home/urls.py
+++ ODA/patient_home/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 
-urlpatterns=[
-    path('',views.home_page, name='pat_home_page'),
+urlpatterns = [
+    path('', views.home_page, name='pat_home_page'),
     path('doctors', views.doctor_list, name='doctor_list_page'),
-    path('doctor_locator',views.doctor_locator, name='doctor_locator'),
-    path('appointments',views.my_appointments, name='my_appointments'),
-    path('blood_banks',views.blood_bank, name='blood_banks'),
-    path('pharma_locator',views.pharma_locator, name='pharma_loc'),
+    path('doctor_locator', views.doctor_locator, name='doctor_locator'),
+    path('appointments', views.my_appointments, name='my_appointments'),
+    path('blood_banks', views.blood_bank, name='blood_banks'),
+    path('pharma_locator', views.pharma_locator, name='pharma_loc'),
+    path('general_medicines', views.gen_meds, name='gen_meds'),
 ]
--- ODA/patient_home/views.py
+++ ODA/patient_home/views.py
@@ -30,9 +30,53 @@ def doctor_locator(request):
 def my_appointments(request):
     return render(request, 'patient_home/my_appo.html')
 
+
 def blood_bank(request):
-    return render(request, 'patient_home/blood_banks.html')
+    lat = Blood_Bank.objects.all().values_list('Lat')
+    long = Blood_Bank.objects.all().values_list('Long')
+    name = Blood_Bank.objects.all().values_list('BB_name')
+    arry2 = []
+    arry1 = []
+    arry3 = []
+    for i in lat:
+        arry1 += [i[0]]
+    for i in long:
+        arry2 += [i[0]]
+    for i in name:
+        arry3 += [i[0]]
+    print(arry1)
+    print(arry2)
+    print('aasfdsv')
+    return render(request, 'patient_home/blood_banks.html', {'lat': arry1, 'lng': arry2, 'names': arry3})
+
 
 def pharma_locator(request):
-    return render(request, 'patient_home/pharma_loc.html')
+    lati = Medical_shop.objects.all().values_list('Lattitude')
+    longi = Medical_shop.objects.all().values_list('Longitude')
+    names = Medical_shop.objects.all().values_list('shop_name')
+    arry2 = []
+    arry1 = []
+    arry3 = []
+    for i in lati:
+        arry1 += [i[0]]
+    for i in longi:
+        arry2 += [i[0]]
+    for i in names:
+        arry3 += [i[0]]
+    return render(request, 'patient_home/pharma_loc.html', {'lat': arry1, 'lng': arry2, 'names': arry3})
+
+
+def gen_meds(request):
+    disease = General_Medicine.objects.all().values_list('Disease')
+    medicine = General_Medicine.objects.all().values_list('Medicine')
+
+    arry4 = []
+    for i in disease:
+        arry4 += [i[0]]
+    arry5 = []
+    for i in medicine:
+        arry5 += [i[0]]
+    print(arry4)
+    print(arry5)
+    return render(request, 'patient_home/gen_meds.html' , {'disease':arry4,'medicine':arry5,'ranges':zip(arry4, arry5)})
 
