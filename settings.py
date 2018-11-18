621e0046aa46bf8212d2d1375b0407901272c311
Yashwanth-kikkuri <37268283+Yashwanth-kikkuri@users.noreply.github.com>
2018-11-18 17:02:26 +0530

    Rest API

--- settings.py
+++ settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diseaseidentification.apps.DiseaseidentificationConfig',
+    'rest_framework',
 
 ]
 
@@ -134,8 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
--- views.py
+++ views.py
@@ -2,6 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from diseaseidentification.models import latlng,tips,General_Medicine,Stock_availability,Medical_shop,Blood_Bank,Blood_avail
 from django.core.mail import send_mail
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from .forms import SignupForm
+from django.core.mail import EmailMessage
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import BloodavailSerializer,StockavailabilitySerializer
+from rest_framework import status
+
+
+
+
+
+
 
 def nearby(request):
     lati=latlng.objects.all().values_list('lat')
@@ -42,14 +64,48 @@ def getlocation(request):
 
 def sendingmail(request):
 
-    send_mail(
-        'Notifying your appointment',
-        'You have an appointment in half an hour http://127.0.0.1:8000/diseaseidentification/tips',
-        'onlinedocapp@gmail.com',
-        ['yashukikkuri@gmail.com'],
-        fail_silently=False,
-    )
-    return render(request,'sendingmail.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            message = render_to_string('activateemail.html', {
+                'user':user,
+                'domain':current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'token': account_activation_token.make_token(user),
+            })
+            mail_subject = 'Activate your blog account.'
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+
+    else:
+        form = SignupForm()
+
+    return render(request, 'sendingmail.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return render(request,'getlocation.html')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+
 
 def medicalshop(request):
     lati=Medical_shop.objects.all().values_list('Lattitude')
@@ -159,3 +215,33 @@ def eye(request):
 def ear(request):
 
     return render(request, 'ear.html')
+
+
+
+class Bloodbanklistview(APIView):
+    def get(self,request):
+        Bloodbank=Blood_avail.objects.all()
+        serializer = BloodavailSerializer(Bloodbank,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = BloodavailSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class Medicalshoplistview(APIView):
+    def get(self,request):
+        Bloodbank=Stock_availability.objects.all()
+        serializer = StockavailabilitySerializer(Bloodbank,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = StockavailabilitySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
