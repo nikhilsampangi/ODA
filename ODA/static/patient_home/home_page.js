94b5cfb8228d18c39464b296ae470d2dffe74a2d
Yashwanth-kikkuri <37268283+Yashwanth-kikkuri@users.noreply.github.com>
2018-11-18 23:43:20 +0530

    updated static files

---
+++ ODA/static/patient_home/home_page.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+$('#ico_home').hover(function(){
+    $('#ico_home').addClass('ico_home_active');
+},function(){
+    $('#ico_home').removeClass('ico_home_active');
+});
+
+$('#ico_appo').hover(function(){
+    $('#ico_appo').addClass('ico_appo_active');
+},function(){
+    $('#ico_appo').removeClass('ico_appo_active');
+});
+
+$('#ico_assi').hover(function(){
+    $('#ico_assi').addClass('ico_assi_active');
+},function(){
+    $('#ico_assi').removeClass('ico_assi_active');
+});
+
+$('#ico_phar').hover(function(){
+    $('#ico_phar').addClass('ico_phar_active');
+},function(){
+    $('#ico_phar').removeClass('ico_phar_active');
+});
+
+$('#ico_abou').hover(function(){
+    $('#ico_abou').addClass('ico_abou_active');
+},function(){
+    $('#ico_abou').removeClass('ico_abou_active');
+});
+
+$('#ico_prof').hover(function(){
+    $('#ico_prof').addClass('ico_prof_active');
+},function(){
+    $('#ico_prof').removeClass('ico_prof_active');
+});
