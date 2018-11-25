23da0ac88ec1757eab7be2cb7ea9df463f4a3f46
Nikhil Sampangi <nikhil.sampangi@gmail.com>
2018-11-26 01:08:14 +0530

    removed stuff

--- ksej_ODA/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from blog.models import *
-
-admin.site.register(textinblog)
-admin.site.register(Doctors)
-admin.site.register(Hospital_Details)
-admin.site.register(Doctor_Type)
--- ksej_ODA/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from blog.models import textinblog
-
-
-class Bloginput(forms.ModelForm):
-    class Meta:
-        model = textinblog
-        fields = ['the_text', 'the_sub']
